a36c50203ab7ff5f36c40e97d6b789736ff934a5
JUAN RAMON PEÑUELAS EMUS <17171456@itculiacan.edu.mx>
2022-02-21 09:11:56 -0700

    Se agrega el modulo de transito

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Productos en tránsito',
+    'version': '13.0.0.1.0',
+    'author': 'JuanEmus',
+    'depends': [
+        'stock',
+        'purchase',
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/transito.xml',
+    ],
+}
